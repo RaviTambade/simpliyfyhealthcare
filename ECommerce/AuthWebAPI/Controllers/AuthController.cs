f5b3519739603466f9cad3cd5a1ef7c593f38c33
Ravi Tambade <ravi.tambade@transflower.in>
2024-11-12 16:59:29 +0530

    membership implementation

---
+++ ECommerce/AuthWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using POCO;
+using Specification;
+using Services;
+using System.Web;
+namespace AuthWebAPI.Controllers
+{
+    public class AuthController : ApiController
+    {
+
+        // POST api/auth
+        public void Post([FromBody]  Credenail crednetial)
+        {
+            IAuthService svc = new AuthService();
+            if (svc.Login(crednetial.Email, crednetial.Password))
+            { 
+                
+            }
+            else
+            {
+
+            }
+
+        }
+
+
+    }
+}
---
+++ ECommerce/Services/AuthService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Specification;
+using POCO;
+
+namespace Services
+{
+    public class AuthService : IAuthService
+    {
+        public bool ForgotPassword(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RestPassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
