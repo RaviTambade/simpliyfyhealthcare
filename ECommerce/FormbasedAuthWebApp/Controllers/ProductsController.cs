d89be06196b03aedca528b68f3abfd5fa8a33287
Ravi Tambade <ravi.tambade@transflower.in>
2024-12-02 19:39:06 +0530

    formbased authentication

---
+++ ECommerce/FormbasedAuthWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormbasedAuthWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        
+        [OutputCache(Duration = 60, VaryByParam = "none")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache(Duration = 60, VaryByParam = "id")]
+        public ActionResult Product(int id)
+        {
+            var product = new
+            {
+                ProductId = id,
+                Title = "Gerbera",
+                UnitPrice = 4
+            };
+            return View(product);
+        }
+
+        public ActionResult List()
+        {
+
+            // Set cache expiration to 60 minutes
+            Response.Cache.SetExpires(DateTime.Now.AddMinutes(60));
+            Response.Cache.SetCacheability(HttpCacheability.Public);
+            Response.Cache.SetValidUntilExpires(true);
+
+            return View();
+        }
+
+
+        [OutputCache(CacheProfile  = "CacheFor60Seconds")]
+        public ActionResult Catalog()
+        {
+            return View();
+        }
+        
+
+    }
+}
---
+++ ECommerceCore/EncryptedPassword/Program.cs
@@ -0,0 +1,20 @@
+﻿using BCrypt.Net;
+// Example password
+string password = "transflower";
+
+// Hash the password with bcrypt
+string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+// Output the hashed password
+Console.WriteLine($"Password: {password}");
+
+Console.WriteLine($"Hashed Password: {hashedPassword}");
+//password = "dfsdfsdf";
+if (hashedPassword != null && BCrypt.Net.BCrypt.Verify(password, hashedPassword))
+{
+    Console.WriteLine("Password is correct.");
+}
+else
+{
+    Console.WriteLine("Invalid password.");
+}
