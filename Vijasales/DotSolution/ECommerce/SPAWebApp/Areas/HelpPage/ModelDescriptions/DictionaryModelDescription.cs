bdcb3cc3f29398774371a0a648f6831d3a22a771
Ravi Tambade <ravi.tambade@transflower.in>
2024-12-04 17:10:00 +0530

    vijay sales solution restructuring

--- Vijasales/DotSolution/ECommerce/SPAWebApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SPAWebApp.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Vijasales/DotSolution/ECommerce/VBWebAPP/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="VBWebAPP.Global_asax" Language="vb" %>
