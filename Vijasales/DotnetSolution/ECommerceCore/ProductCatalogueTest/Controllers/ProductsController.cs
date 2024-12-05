c675e1cb2cd7faf7b9f91fb4eb413a30f5df61aa
Rutuja Yadav Patil <rutuja.patil@simplifyhealthcare.com>
2024-12-05 21:47:43 +0000

    Products Detail View Is added

--- Vijasales/DotnetSolution/ECommerceCore/ProductCatalogueTest/Controllers/ProductsController.cs
+++ Vijasales/DotnetSolution/ECommerceCore/ProductCatalogueTest/Controllers/ProductsController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductCatalogueTest.Models;
-using System.ComponentModel.Design;
+
 
 namespace ProductCatalogueTest.Controllers
 {
     public class ProductsController : Controller
     {
+        // Sample data - ideally, this would come from a database
+        private static readonly List<CardModel> cardList = new List<CardModel>
+        {
+            new CardModel { Id = 1, ImageUrl = "/Images/galaxy_M05.jpg", Title = "Galaxy M05", Price = 10000, Description = "This is Galaxy M05 with great features", Stock = 10 },
+            new CardModel { Id = 2, ImageUrl = "/Images/galaxy_S22.jpg", Title = "Galaxy S22", Price = 50000, Description = "Galaxy S22 with top notch performance", Stock = 5 },
+            new CardModel { Id = 3, ImageUrl = "/Images/galaxy_S23.jpg", Title = "Galaxy S23", Price = 55000, Description = "Galaxy S23 with the latest technology", Stock = 8 },
+            new CardModel { Id = 4, ImageUrl = "/Images/galaxy_M05.jpg", Title = "Galaxy M05", Price = 10000, Description = "This is Galaxy M05 with great features", Stock = 10 },
+            new CardModel { Id = 5, ImageUrl = "/Images/galaxy_S22.jpg", Title = "Galaxy S22", Price = 50000, Description = "Galaxy S22 with top notch performance", Stock = 5 },
+            new CardModel { Id = 6, ImageUrl = "/Images/galaxy_S23.jpg", Title = "Galaxy S23", Price = 55000, Description = "Galaxy S23 with the latest technology", Stock = 8 }
+        };
+
         public IActionResult Index()
         {
             return View();
@@ -13,24 +24,14 @@ namespace ProductCatalogueTest.Controllers
 
         public IActionResult GridLayout()
         {
-
             return View();
         }
 
         [HttpGet]
         public IActionResult Grid()
         {
-            var cardList = new List<CardModel>
-            {
-                new CardModel { ImageUrl = "/Images/galaxy_M05.jpg", Title = "Galaxy M05 - View 1",Price=10000 },
-                new CardModel { ImageUrl = "/Images/galaxy_S22.jpg", Title = "Galaxy S22 - View 1",Price=50000 },
-                new CardModel { ImageUrl = "/Images/galaxy_S23.jpg", Title = "Galaxy S23 - View 1",Price=55000 },
-                new CardModel { ImageUrl = "/Images/galaxy_M05.jpg", Title = "Galaxy M05 - View 1",Price=10000 },
-                new CardModel { ImageUrl = "/Images/galaxy_S22.jpg", Title = "Galaxy S22 - View 1",Price=50000 },
-                new CardModel { ImageUrl = "/Images/galaxy_S23.jpg", Title = "Galaxy S23 - View 1",Price=55000 },
-
-             };
-                return Json(cardList);
+            // Return the list of products in JSON format
+            return Json(cardList);
         }
 
         public IActionResult CardLayout()
@@ -38,7 +39,17 @@ namespace ProductCatalogueTest.Controllers
             return View();
         }
 
+        // The Details action now takes an ID parameter to fetch the correct product
+        public IActionResult Details(int id)
+        {
+            var product = cardList.FirstOrDefault(p => p.Id == id);
 
+            if (product == null)
+            {
+                return NotFound(); // Return a 404 if the product is not found
+            }
+
+            return View(product); // Pass the product to the view for display
+        }
     }
-
 }
