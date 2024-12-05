c0b8d8f73f4e3c638f7851fbdca672516596640d
Yash Valvi <yash.valvi@simplifyhealthcare.com>
2024-12-05 11:30:23 +0000

    navbar

--- Vijasales/DotnetSolution/ECommerceCore/ShipmentTest/Program.cs
+++ Vijasales/DotnetSolution/ECommerceCore/ShipmentTest/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ShipmentLib.Entities;
+using ShipmentLib.Repositories;
+using ShipmentLib.Repositories.ORM;
+
+
 Console.WriteLine("TESTING SHIPMENT!");
 
+
+IShipmentRepository repo = new ShipmentRepository();
+
+
+foreach( Shipment sh in repo.GetAll())
+{
+    Console.WriteLine($"{sh.Id} {sh.ShipmentDate}, status - {sh.ShipmentStatus}" );
+}
