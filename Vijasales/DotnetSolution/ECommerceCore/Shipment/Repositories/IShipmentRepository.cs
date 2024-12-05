390f7ffd6fb8560ab7196aa145f8005a855b7fa5
Aditya Jamdade <aditya.jamdade@simplifyhealthcare.com>
2024-12-05 10:12:09 +0000

    definition of ishipmentrepository

--- Vijasales/DotnetSolution/ECommerceCore/Shipment/Repositories/IShipmentRepository.cs
+++ Vijasales/DotnetSolution/ECommerceCore/Shipment/Repositories/IShipmentRepository.cs
@@ -9,7 +9,21 @@ namespace ShipmentLib.Repositories
 {
     public interface IShipmentRepository
     {
+        List<Shipment> GetAll();
+
+        List<Shipment> GetByDate(DateTime date);
+
+        List<Shipment> GetByStatus(string status);
+
+        // id refers to shipment id
+        bool Delete(int id);
+
+        Shipment GetById(int id);
+
         bool Create(Shipment shipment);
+
+        bool Update(Shipment shipment);
+
 
     }
 }
