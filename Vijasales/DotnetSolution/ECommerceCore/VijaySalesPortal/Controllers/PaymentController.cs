a356b8a1d9b80e2edec27bf5fa94f9ad097c3734
Ruchita Amale <ruchita.amale@simplifyhealthcare.com>
2024-12-06 11:42:12 +0000

    Payments controller added

---
+++ Vijasales/DotnetSolution/ECommerceCore/VijaySalesPortal/Controllers/PaymentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VijaySalesPortal.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PayNow()
+        {
+            return View();
+        }
+    }
+}
