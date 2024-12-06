ddedcef466c635c541869133f5ea9ee5743eeecf
Chetan More <chetan.more@simplifyhealthcare.com>
2024-12-06 07:04:33 +0000

    Api Controller Added

---
+++ Vijasales/DotnetSolution/ECommerceCore/ShoppingCart/Entities/Cart.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoppingCart.Entities
+{
+    public class Cart
+    {
+        public List<Items> Items { get; set; }
+        public Cart()
+        {
+            Items = new List<Items>();
+        }
+
+    }
+}
--- Vijasales/DotnetSolution/ECommerceCore/VijaySalesAPI/Program.cs
+++ Vijasales/DotnetSolution/ECommerceCore/VijaySalesAPI/Program.cs
@@ -1,14 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Add in-memory distributed cache
+builder.Services.AddDistributedMemoryCache();  // This is the key line for in-memory cache
+
+// Add session service
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = ".ShoppingCart.Session";
+    options.IdleTimeout = TimeSpan.FromMinutes(600);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseAuthorization();
+
+// Enable session middleware after routing
+app.UseSession();
 
 app.MapControllers();
 
