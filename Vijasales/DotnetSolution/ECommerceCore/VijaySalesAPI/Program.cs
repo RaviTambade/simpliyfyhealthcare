a3658ebf96b4b5b89d66efeced4c550788158af6
Ravi Tambade <ravi.tambade@transflower.in>
2024-12-06 13:19:26 +0530

    cors policy setting

--- Vijasales/DotnetSolution/ECommerceCore/VijaySalesAPI/Program.cs
+++ Vijasales/DotnetSolution/ECommerceCore/VijaySalesAPI/Program.cs
@@ -1,12 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseAuthorization();
 
