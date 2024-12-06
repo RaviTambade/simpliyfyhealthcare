5238f549775b6923dead8ce3f8aa18e4d18e6200
Rutuja Yadav Patil <rutuja.patil@simplifyhealthcare.com>
2024-12-06 09:33:27 +0000

    API For GetAll Implemented

--- Vijasales/DotnetSolution/ECommerceCore/VijaySalesAPI/Program.cs
+++ Vijasales/DotnetSolution/ECommerceCore/VijaySalesAPI/Program.cs
@@ -1,10 +1,13 @@
+using Catalog.Repositories;
+using Catalog.Repositories.Connected;
+using Catalog.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-<<<<<<< HEAD
+
 builder.Services.AddCors();
-=======
->>>>>>> 35a7b6257c5a6a69316773db50557691439ef819
+
 builder.Services.AddControllers();
 
 // Add in-memory distributed cache
@@ -19,14 +22,21 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+// Adding services which are needed in the future
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+// Register IDataRepository and ProductRepository for dependency injection
+builder.Services.AddTransient<IDataRepository, ProductRepository>();
+
+// Register ProductService (already done in your code)
+builder.Services.AddTransient<IProductService, ProductService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-<<<<<<< HEAD
+
 app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
-=======
->>>>>>> 35a7b6257c5a6a69316773db50557691439ef819
 app.UseAuthorization();
 
 // Enable session middleware after routing
