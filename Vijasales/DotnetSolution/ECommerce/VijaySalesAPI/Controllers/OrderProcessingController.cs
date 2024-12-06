efb12653af398c357d9c77650669bdfa4e253ae5
Shruti Kadam <shruti.kadam@simplifyhealthcare.com>
2024-12-06 11:25:52 +0000

    GetAll,GetById API Test

--- Vijasales/DotnetSolution/ECommerce/VijaySalesAPI/Controllers/OrderProcessingController.cs
+++ Vijasales/DotnetSolution/ECommerce/VijaySalesAPI/Controllers/OrderProcessingController.cs
@@ -4,10 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using OrdersProcessing.Services.ORM;
+using OrdersProcessing.Entities;
+using OrdersProcessing.Repositories.ORM;
+using System.Web.UI.WebControls;
 
 namespace VijaySalesAPI.Controllers
 {
     public class OrderProcessingController : ApiController
     {
+        // GET api/values
+
+        public IEnumerable<Order> Get()
+        {
+            IOrderService svc = new OrderService();
+            List<Order> orders = svc.GetAll();
+            return orders;
+        }
+
+
+        public Order Get(int id) 
+        {
+            IOrderService svc = new OrderService(); 
+            Order order= svc.GetOrder(id);
+            return  order;
+
+        }
     }
 }
