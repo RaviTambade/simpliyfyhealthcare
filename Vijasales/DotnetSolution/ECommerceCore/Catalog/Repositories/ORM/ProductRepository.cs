7043b1ef6659d8cca7517f9d0cc22b19ab33202e
Yash Valvi <yash.valvi@simplifyhealthcare.com>
2024-12-06 12:03:01 +0000

    ORM

---
+++ Vijasales/DotnetSolution/ECommerceCore/Catalog/Repositories/ORM/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ProductRepository
+    {
+        public List<Product> GetAll()
+        {
+            using (var ctx = new ProductContext())
+            {
+                var products = ctx.Products.ToList();
+                return products;
+            }
+        }
+    }
+}
