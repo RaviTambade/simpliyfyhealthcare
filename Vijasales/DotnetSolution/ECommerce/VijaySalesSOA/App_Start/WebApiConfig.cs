74178bab6cfe829776897a7f0924a3937044a76d
Samrudhee Deshmukh <samrudhee.deshmukh@simplifyhealthcare.com>
2024-12-09 11:23:27 +0000

    shoppingCart in .net

--- Vijasales/DotnetSolution/ECommerce/VijaySalesSOA/App_Start/WebApiConfig.cs
+++ Vijasales/DotnetSolution/ECommerce/VijaySalesSOA/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Http;
 
 namespace VijaySalesSOA
@@ -10,7 +11,7 @@ namespace VijaySalesSOA
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
 
---
+++ Vijasales/DotnetSolution/ECommerce/VijaySalesSOA/Controllers/CartController.cs
@@ -0,0 +1,41 @@
+﻿using Catalog.Entities;
+using ShoppingCart.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace VijaySalesSOA.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            Cart theCart = (Cart)this.Session["cart"];
+
+            return Json(theCart,JsonRequestBehavior.AllowGet);
+            }
+
+        [System.Web.Http.HttpPost]
+        public ActionResult AddtoCart([FromBody]Product product) {
+            var item = new Items
+
+            {
+
+                ProductId = product.Id,
+
+                Quantity = 1,
+
+                Price = product.Price
+
+            };
+            Cart theCart = (Cart)this.Session["cart"];
+            theCart.Items.Add(item);
+            return Json(theCart, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
