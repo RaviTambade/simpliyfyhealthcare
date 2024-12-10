b89b31f0af2eb4f0f3910a88ee6ba5e5a8a0ce26
Shriwatsal Patil <shriwatsal.patil@simplifyhealthcare.com>
2024-12-10 10:01:36 +0000

    ShipmentRepo Foreach not used

--- Vijasales/DotnetSolution/ECommerce/Shipment/Repositories/ORM/ShipmentRepository.cs
+++ Vijasales/DotnetSolution/ECommerce/Shipment/Repositories/ORM/ShipmentRepository.cs
@@ -41,92 +41,42 @@ namespace Shipment.Repositories.ORM
                     await context.SaveChangesAsync();
                     status = true;
                 }
-                else
-                {
-                    Console.WriteLine("Shipment not found.");
-                }
             }
-
             return status;
         }
 
         public async Task<List<Delivery>> GetAllAsync()
         {
-            //// changes needed
-            List<Delivery> shipments = new List<Delivery>();
             using (var context = new ShipmentContext())
             {
                 var dbshipments = await context.Shipments.ToListAsync();
-                foreach (var shipment in dbshipments)
-                {
-                    Delivery theShipment = new Delivery();
-                    theShipment.Id = shipment.Id;
-                    theShipment.OrderId = shipment.OrderId;
-                    theShipment.ShipmentDate = shipment.ShipmentDate;
-                    theShipment.Status = shipment.Status;
-
-                    shipments.Add(theShipment);
-                }
+                return dbshipments;
             }
-            return shipments;
         }
 
         public async Task<List<Delivery>> GetByDateAsync(DateTime date)
         {
-
-            List<Delivery> shipments = new List<Delivery>();
-
             using (var context = new ShipmentContext())
             {
-                var dbshipments = context.Shipments.ToList();
-
-                foreach (var shipment in dbshipments)
-                {
-                    Delivery theShipment = new Delivery();
-                    if (shipment.ShipmentDate == date)
-                    {
-                        theShipment.Id = shipment.Id;
-                        theShipment.OrderId = shipment.OrderId;
-                        theShipment.ShipmentDate = shipment.ShipmentDate;
-                        theShipment.Status = shipment.Status;
-
-                        shipments.Add(theShipment);
-                    }
-
-                }
-                return shipments;
-
+                var dbShipments = await context.Shipments
+                    .Where(s => s.ShipmentDate == date)
+                    .ToListAsync();
+                return dbShipments;
             }
         }
 
         public async Task<List<Delivery>> GetByDateAsync(DateTime startdate, DateTime enddate)
         {
-            List<Delivery> shipments = new List<Delivery>();
-
             using (var context = new ShipmentContext())
             {
-                // Query to get shipments within the date range
                 var dbShipments = await context.Shipments
                     .Where(s => s.ShipmentDate >= startdate && s.ShipmentDate <= enddate)
                     .ToListAsync();
 
-                foreach (var shipment in dbShipments)
-                {
-                    // Mapping the shipment to Delivery object
-                    Delivery theShipment = new Delivery
-                    {
-                        Id = shipment.Id,
-                        OrderId = shipment.OrderId,
-                        ShipmentDate = shipment.ShipmentDate,
-                        Status = shipment.Status
-                    };
-
-                    shipments.Add(theShipment);
-                }
+                return dbShipments;
             }
-
-            return shipments;
         }
+
 
         public async Task<ShipmentDetail> GetByIdAsync(int shipmentId)
         {
@@ -162,34 +112,15 @@ namespace Shipment.Repositories.ORM
 
             return shipmentDetail.DeliveryStatus;
         }
-
-
-            public async Task<List<Delivery>> GetByStatusAsync(string status)
+        public async Task<List<Delivery>> GetByStatusAsync(string status)
         {
-            List<Delivery> shipments = new List<Delivery>();
 
             using (var context = new ShipmentContext())
             {
-                var dbshipments = await context.Shipments.ToListAsync();
-
-                foreach (var shipment in dbshipments)
-                {
-                    Delivery theShipment = new Delivery();
-                    if (shipment.Status == status)
-                    {
-                        theShipment.Id = shipment.Id;
-                        theShipment.OrderId = shipment.OrderId;
-                        theShipment.ShipmentDate = shipment.ShipmentDate;
-                        theShipment.Status = shipment.Status;
-
-                        shipments.Add(theShipment);
-                    }
-                }
-
-                return shipments;
+                var dbshipments = await context.Shipments
+                    .Where(s => s.Status == status).ToListAsync();
+                return dbshipments;
             }
-
-
         }
 
         public async Task<bool> UpdateAsync(Delivery shipment)
@@ -208,14 +139,28 @@ namespace Shipment.Repositories.ORM
                     await context.SaveChangesAsync();
                     status = true;
                 }
-                else
-                {
-                    Console.WriteLine("Shipment not found.");
-                }
+
             }
 
-
             return status;
+        }
+
+        public async Task<bool> UpdateStatusAsync(int id, string shipmentStatus)
+        {
+            bool status = false;
+            using (var context = new ShipmentContext())
+            {
+                var foundShipment = await context.Shipments.SingleOrDefaultAsync(s => s.Id == id);
+                if (foundShipment != null)
+                {
+                    // Only update the status
+                    foundShipment.Status = shipmentStatus;
+                    await context.SaveChangesAsync();
+                    status = true;
+                }
+            }
+            return status;
+
         }
 
 
