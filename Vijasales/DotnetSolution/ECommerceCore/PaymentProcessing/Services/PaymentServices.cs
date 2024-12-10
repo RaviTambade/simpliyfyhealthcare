6224afcecf20304cd1d82b433c515165bd252ff9
Ruchita Amale <ruchita.amale@simplifyhealthcare.com>
2024-12-10 06:31:10 +0000

    Changes in Payments processing

--- Vijasales/DotnetSolution/ECommerceCore/PaymentProcessing/Services/PaymentServices.cs
+++ Vijasales/DotnetSolution/ECommerceCore/PaymentProcessing/Services/PaymentServices.cs
@@ -42,6 +42,9 @@ namespace PaymentProcessing.Services
             payment.PaymentStatus = status;
             payment.TransactionId = transactionId;
             payment.PaymentAmount = amount;
+            DateTime currentDate = DateTime.Now.Date;
+            string formattedDate = currentDate.ToString("yyyy-MM-dd");
+            payment.PaymentDate = formattedDate;
             // Update the Payment record with the new information
             bool updateSuccess = await _repo.UpdateAsync(payment);
             return updateSuccess;
