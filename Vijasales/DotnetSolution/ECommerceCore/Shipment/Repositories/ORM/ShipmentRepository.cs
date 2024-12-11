171c2531c593fddc491dd887dcf5571e77fae55a
Shriwatsal Patil <shriwatsal.patil@simplifyhealthcare.com>
2024-12-11 07:27:54 +0000

    Core shipment

--- Vijasales/DotnetSolution/ECommerceCore/Shipment/Repositories/ORM/ShipmentRepository.cs
+++ Vijasales/DotnetSolution/ECommerceCore/Shipment/Repositories/ORM/ShipmentRepository.cs
@@ -134,7 +134,7 @@ namespace Shipment.Repositories.ORM
         public async Task<ShipmentDetail> GetByIdAsync(int shipmentId)
         {
 
-<<<<<<< HEAD
+
                 ShipmentDetail shipmentDetail = null;
                 using (var context = new ShipmentContext(_configuration))
                 {
@@ -148,22 +148,6 @@ namespace Shipment.Repositories.ORM
                 }
 
                 return shipmentDetail;
-=======
-            ShipmentDetail shipmentDetail = null;
-            using (var context = new ShipmentContext(_configuration))
-            {
-                // Define the stored procedure query with the necessary parameter
-                var query = @"EXEC GetShipmentDetails @ShipmentId";
-                var param = new SqlParameter("@ShipmentId", shipmentId);
-
-                shipmentDetail = context.Set<ShipmentDetail>()
-                           .FromSqlRaw(query, param)
-                           .AsEnumerable()
-                           .FirstOrDefault();
->>>>>>> d896072a549b003f79eccb1ecb8a936f1c61af95
-            }
-
-            return shipmentDetail;
 
         }
 
@@ -196,10 +180,7 @@ namespace Shipment.Repositories.ORM
                 var dbShipments = await context.Shipments
                     .Where(s => s.OrderId == orderId)
                     .ToListAsync();
-<<<<<<< HEAD
-=======
 
->>>>>>> 5cb31ec77b5792d08c1cbffb46d7aea0ac1a310e
                 return dbShipments[0]?.Status ?? "Shipment status not found.";
             }
         }
