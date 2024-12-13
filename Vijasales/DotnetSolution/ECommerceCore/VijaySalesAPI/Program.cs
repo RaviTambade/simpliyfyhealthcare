e648170919a3b04ff1ef9c03944bdfd70cfaba37
adityajamdade <adijamdade156@gmail.com>
2024-12-13 07:22:35 +0000

    temp changes

--- Vijasales/DotnetSolution/ECommerceCore/VijaySalesAPI/Program.cs
+++ Vijasales/DotnetSolution/ECommerceCore/VijaySalesAPI/Program.cs
@@ -1,9 +1,5 @@
 using Catalog.Repositories;
 using Catalog.Repositories.ORM;
-<<<<<<< HEAD
-=======
-using Catalog.Repositories.Review.Connected;
->>>>>>> 275ee7bee7e7e416cab913dd763ff60619e0a12a
 using Catalog.Services;
 using CRM.Repositories.ORM;
 using CRM.Repositories;
@@ -13,20 +9,8 @@ using PaymentProcessing.Repositories.Connected;
 using OrderProcessing.Repositories.Connected;
 using OrderProcessing.Services;
 using OrderProcessing.Services.Connected;
-<<<<<<< HEAD
 using Banking.Repositories.Connected;
 using Banking.Services;
-=======
-
-<<<<<<< HEAD
-=======
-
-
-
-
-
->>>>>>> 7bdace22d4110bb3d70f8987710689eff7aa12a2
->>>>>>> 275ee7bee7e7e416cab913dd763ff60619e0a12a
 using Shipment.Repositories;
 using Shipment.Repositories.ORM;
 using Shipment.Services;
@@ -34,19 +18,15 @@ using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using VijaySalesAPI.Helper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-
 var builder = WebApplication.CreateBuilder(args);
-
-
-
+// Add services to the container.
 builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddDistributedMemoryCache();  // This is the key line for in-memory cache
 // Configure AppSettings section from configuration
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 
-
-
+// Add session service
 builder.Services.AddSession(options =>
 {
     options.Cookie.Name = "ShoppingCart.Session";
@@ -55,105 +35,49 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
     options.Cookie.SameSite = SameSiteMode.Lax;
     options.Cookie.SecurePolicy = CookieSecurePolicy.None;
-
 });
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocalhost", policy =>
     {
-
-<<<<<<< HEAD
         policy.WithOrigins("http://localhost:5260", "http://localhost:5284", "http://localhost:5218") // Allow your frontend's URL
-=======
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 7bdace22d4110bb3d70f8987710689eff7aa12a2
- 
-
-        policy.WithOrigins("http://localhost:5260", "http://localhost:5284", "http://localhost:5218")  // Allow your frontend's URL
-
-
-<<<<<<< HEAD
-=======
-
-        
-
->>>>>>> 275ee7bee7e7e416cab913dd763ff60619e0a12a
-
-
->>>>>>> 7bdace22d4110bb3d70f8987710689eff7aa12a2
               .AllowAnyHeader()  // Allow any headers
               .AllowAnyMethod()  // Allow any HTTP methods (GET, POST, etc.)
               .AllowCredentials();  // Allow cookies and credentials to be sent
     });
 });
-
 // Adding services which are needed in the future
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
 // Register IDataRepository 
 builder.Services.AddTransient<IUserDataRepository, UserRepository>();
 builder.Services.AddTransient<IUserService, UserService>();
-
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddTransient<IProductService, ProductService>();
-
 builder.Services.AddTransient<IOrderRepository, OrderRepository>();
 builder.Services.AddTransient<IOrderItemRepository, OrderItemRepository>();
 builder.Services.AddTransient<IOrderItemService, OrderItemService>();
 builder.Services.AddTransient<IOrderService, OrderService>();
-
 builder.Services.AddTransient<IPaymentRepository, PaymentRepository>();
 builder.Services.AddTransient<IPaymentServices, PaymentServices>();
-
 builder.Services.AddTransient<IShipmentService, ShipmentService>();
 builder.Services.AddTransient<IShipmentRepository, ShipmentRepository>();
 
-
 builder.Services.AddTransient<ICardRepository, CardRepository>();
 builder.Services.AddTransient<ICardService, CardServices>();
-
 builder.Services.AddTransient<IBankRepository, BankRepository>();
-
 builder.Services.AddTransient<IBankService, BankService>();
-
-<<<<<<< HEAD
-=======
-
-var app = builder.Build();
-app.UseCors("AllowLocalhost");
-app.UseRouting();
-
-
-
-app.UseAuthorization();
-app.UseSession();
-app.MapControllers();
-
-
-app.Run();
-
-
->>>>>>> 275ee7bee7e7e416cab913dd763ff60619e0a12a
 builder.Services.AddTransient<IAuthService, AuthService>();
 builder.Services.AddTransient<IUserDataRepository, UserRepository>();
-
 //JWT Setup
-
 // Configure JWT Authentication
 var appSettingsSection = builder.Configuration.GetSection("AppSettings");
 var appSettings = appSettingsSection.Get<AppSettings>();
 var key = Encoding.ASCII.GetBytes(appSettings.Secret);
 
-
 builder.Services.AddAuthentication(x =>
 {
     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
 }).AddCookie(options =>
 {
     options.Cookie.Name = "YourAppAuthCookie"; // Set cookie name
@@ -161,7 +85,6 @@ builder.Services.AddAuthentication(x =>
     options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; // Use secure cookies in production (set to Always if HTTPS is used)
     options.Cookie.SameSite = SameSiteMode.Strict; // Helps to prevent CSRF attacks
     options.SlidingExpiration = true; // Cookie expires after a set time but is renewed with activity
-
 })
 .AddJwtBearer(x =>
 {
@@ -175,12 +98,9 @@ builder.Services.AddAuthentication(x =>
         ValidateAudience = false
     };
 
-
 });
 
-
 var app = builder.Build();
-
 
 app.UseCors("AllowLocalhost");
 app.UseRouting();
@@ -189,18 +109,4 @@ app.UseHttpsRedirection();
 app.UseAuthentication();  // Make sure this comes before UseAuthorization
 app.UseAuthorization();
 app.MapControllers();
-
-<<<<<<< HEAD
 app.Run();
-=======
-
-app.Run();
-
-
-
-
-
-
-
-
->>>>>>> 275ee7bee7e7e416cab913dd763ff60619e0a12a
